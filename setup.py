7e663f9da8aeb121597eb86d5a21f037e2db3324
Subhankar Panda <91069648+Wanderer-of-the-abyss@users.noreply.github.com>
2023-06-15 17:29:33 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nbdt',
+    version='0.1',
+    author='Subhankar Panda',
+    author_email='subhankarpanda556@example.com',
+    description='nbdt library for reccomending authors, papers, and journals',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+       
+    ],
+)
